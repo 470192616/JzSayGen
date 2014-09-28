85aaa0f6d2bd735bebec829a97432aa675797835
jiangzheng <470192616@qq.com>
2014-09-28 21:02:19 +0800

    enum flag

--- JzSayGen/EnumExten.cs
+++ JzSayGen/EnumExten.cs
@@ -33,27 +33,108 @@ namespace JzSayGen
     public static class EnumExten
     {
         /// <summary>
+        /// 枚举转dictionary类型
+        /// </summary>
+        private static Dictionary<Type, Dictionary<Int32, string>> EnumDictionaryCache = new Dictionary<Type, Dictionary<Int32, string>>();
+
+        /// <summary>
+        /// 判断是否枚举成员值
+        /// </summary>
+        /// <param name="enumType"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsDefinedInt32(Type enumType, Int32 value)
+        {
+            return System.Enum.IsDefined(enumType, value);
+        }
+
+        /// <summary>
+        /// int32 转换成枚举
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static T ParseInt32<T>(Int32 value)
+        {
+            return (T)Enum.ToObject(typeof(T), value);
+        }
+
+        /// <summary>
+        /// 带EnumTip属性的枚举转dictionary类型
+        /// </summary>
+        /// <param name="enumType"></param>
+        /// <returns></returns>
+        public static Dictionary<Int32, string> GetDictionary(Type enumType)
+        {
+            if (EnumDictionaryCache.ContainsKey(enumType)) return EnumDictionaryCache[enumType];
+
+            Dictionary<Int32, string> dit = new Dictionary<int, string>();
+            foreach (int i in Enum.GetValues(enumType))
+            {
+                string enumName = Enum.GetName(enumType, i);
+                object[] attrs = enumType.GetField(enumName).GetCustomAttributes(typeof(EnumTipAttribute), true);
+                if (attrs != null && attrs.Length == 1)
+                {
+                    EnumTipAttribute desc = attrs[0] as EnumTipAttribute;
+                    if (desc != null) enumName = desc.Tip;
+                }
+                dit.Add(i, enumName);
+            }
+            EnumDictionaryCache.Add(enumType, dit);
+            return dit;
+        }
+
+        /// <summary>
         /// 获取位域的值列表
         /// </summary>
         /// <param name="e"></param>
         /// <returns></returns>
-        public static int[] ToIntVal(this Enum e)
+        public static int[] GetFlagsInt32List(this Enum e)
         {
             var et = e.GetType();
             var arr = e.ToString().Split(',');
-            var tmp = new int[arr.Length];
+            var tmp = new Int32[arr.Length];
             for (int i = 0, j = arr.Length; i < j; i++)
             {
-                var ddc = Enum.Parse(et, arr[i]);
-                tmp[i] = (int)Enum.Parse(et, arr[i]);
+                tmp[i] = (Int32)Enum.Parse(et, arr[i]);
             }
             return tmp;
         }
 
         /// <summary>
-        /// 
+        /// 数字值缓存
         /// </summary>
-        private static Dictionary<Enum, string> dictDiscs = new Dictionary<Enum, string>();
+        private static Dictionary<Enum, Int32> EnumInt32Cache = new Dictionary<Enum, Int32>();
+
+        /// <summary>
+        /// 获取数字值
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        public static Int32 GetInt32(this Enum e)
+        {
+            if (EnumInt32Cache.ContainsKey(e)) return EnumInt32Cache[e];
+
+            var et = e.GetType();
+            var ev = (Int32)Enum.Parse(et, e.ToString());
+            EnumInt32Cache.Add(e, ev);
+            return ev;
+        }
+
+        /// <summary>
+        /// 获取数字的字符串值
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        public static string GetInt32Str(this Enum e)
+        {
+            return e.GetInt32().ToString();
+        }
+
+        /// <summary>
+        /// enumTip属性值缓存
+        /// </summary>
+        private static Dictionary<Enum, string> EnumTipCache = new Dictionary<Enum, string>();
 
         /// <summary>
         /// 获取枚举成员说明
@@ -62,10 +143,10 @@ namespace JzSayGen
         /// <returns></returns>
         public static string GetTip(this Enum e)
         {
-            if (dictDiscs.ContainsKey(e)) return dictDiscs[e];
+            if (EnumTipCache.ContainsKey(e)) return EnumTipCache[e];
 
             string strDisc = _GetTip(e);
-            dictDiscs.Add(e, strDisc);
+            EnumTipCache.Add(e, strDisc);
             return strDisc;
         }
 
--- JzSayGen/XmlToModel.cs
+++ JzSayGen/XmlToModel.cs
@@ -16,6 +16,20 @@ namespace JzSayGen
         /// 解析节点列表
         /// </summary>
         /// <typeparam name="T">返回的实体类型</typeparam>
+        /// <param name="xmlStr">xml字符串</param>
+        /// <param name="xpath">查询节点 /root/friends/friend</param>
+        /// <returns></returns>
+        public static List<T> ParseNodeList<T>(string xmlStr, string xpath) where T : class, new()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xmlStr);
+            return ParseNodeList<T>(doc, xpath);
+        }
+
+        /// <summary>
+        /// 解析节点列表
+        /// </summary>
+        /// <typeparam name="T">返回的实体类型</typeparam>
         /// <param name="doc">xml对象</param>
         /// <param name="xpath">查询节点 /root/friends/friend</param>
         /// <returns></returns>
@@ -39,6 +53,20 @@ namespace JzSayGen
                 lt.Add(ParseNode<T>(node));
             }
             return lt;
+        }
+
+        /// <summary>
+        /// 解析节点属性
+        /// </summary>
+        /// <param name="xmlStr">xml字符串</param>
+        /// <param name="xpath">查询节点 /root/user</param>
+        /// <param name="attributes">属性列表</param>
+        /// <returns></returns>
+        public static Dictionary<string, string> ParseNode(string xmlStr, string xpath, List<string> attributes)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xmlStr);
+            return ParseNode(doc, xpath, attributes);
         }
 
         /// <summary>
@@ -75,10 +103,24 @@ namespace JzSayGen
         /// 解析节点属性
         /// </summary>
         /// <typeparam name="T">返回的实体类型</typeparam>
+        /// <param name="xmlStr">xml字符串</param>
+        /// <param name="xpath">查询节点 /root/user</param>
+        /// <returns></returns>
+        public static T ParseNode<T>(string xmlStr, string xpath) where T : class, new()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xmlStr);
+            return ParseNode<T>(doc, xpath);
+        }
+
+        /// <summary>
+        /// 解析节点属性
+        /// </summary>
+        /// <typeparam name="T">返回的实体类型</typeparam>
         /// <param name="doc">xml对象</param>
         /// <param name="xpath">查询节点 /root/user</param>
         /// <returns></returns>
-        public static T ParseNode<T>(XmlDocument doc, string xpath) where T : class,new()
+        public static T ParseNode<T>(XmlDocument doc, string xpath) where T : class, new()
         {
             XmlNode node = doc.SelectSingleNode(xpath);
             return ParseNode<T>(node);
