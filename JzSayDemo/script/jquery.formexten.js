d114ff8c76a9bbd1dee983baf10de4d317b113ac
jiangzheng <470192616@qq.com>
2014-10-23 23:51:48 +0800

    login

--- JzSayDemo/script/jquery.formexten.js
+++ JzSayDemo/script/jquery.formexten.js
@@ -36,10 +36,7 @@ $.fn.ruleSingleCheckbox = function () {
         var checkObj = parentObj.children('input:checkbox').eq(0);
         parentObj.children().hide();
         //添加元素及样式
-        var newObj = $('<a href="javascript:;">'
-		+ '<i class="off">否</i>'
-		+ '<i class="on">是</i>'
-		+ '</a>').prependTo(parentObj);
+        var newObj = $('<a href="javascript:;"><i class="off">否</i><i class="on">是</i></a>').prependTo(parentObj);
         parentObj.addClass("single-checkbox");
         //判断是否选中
         if (checkObj.prop("checked") == true) {
@@ -53,11 +50,9 @@ $.fn.ruleSingleCheckbox = function () {
         //绑定事件
         $(newObj).click(function () {
             if ($(this).hasClass("selected")) {
-                $(this).removeClass("selected");
-                //checkObj.prop("checked", false);
+                $(this).removeClass("selected");                
             } else {
-                $(this).addClass("selected");
-                //checkObj.prop("checked", true);
+                $(this).addClass("selected");                
             }
             checkObj.trigger("click"); //触发对应的checkbox的click事件
         });
@@ -87,14 +82,11 @@ $.fn.ruleMultiCheckbox = function () {
             //绑定事件
             $(newObj).click(function () {
                 if ($(this).hasClass("selected")) {
-                    $(this).removeClass("selected");
-                    //parentObj.find(':checkbox').eq(indexNum).prop("checked",false);
+                    $(this).removeClass("selected");                    
                 } else {
-                    $(this).addClass("selected");
-                    //parentObj.find(':checkbox').eq(indexNum).prop("checked",true);
+                    $(this).addClass("selected");                    
                 }
-                parentObj.find(':checkbox').eq(indexNum).trigger("click"); //触发对应的checkbox的click事件
-                //alert(parentObj.find(':checkbox').eq(indexNum).prop("checked"));
+                parentObj.find(':checkbox').eq(indexNum).trigger("click"); //触发对应的checkbox的click事件               
             });
         });
     };
@@ -128,8 +120,7 @@ $.fn.ruleMultiPorp = function () {
                 } else {
                     $(this).parent().addClass("selected");
                 }
-                parentObj.find(':checkbox').eq(indexNum).trigger("click"); //触发对应的checkbox的click事件
-                //alert(parentObj.find(':checkbox').eq(indexNum).prop("checked"));
+                parentObj.find(':checkbox').eq(indexNum).trigger("click"); //触发对应的checkbox的click事件                
             });
         });
     };
@@ -161,8 +152,7 @@ $.fn.ruleMultiRadio = function () {
                 $(this).addClass("selected");
                 parentObj.find('input[type="radio"]').prop("checked", false);
                 parentObj.find('input[type="radio"]').eq(indexNum).prop("checked", true);
-                parentObj.find('input[type="radio"]').eq(indexNum).trigger("click"); //触发对应的radio的click事件
-                //alert(parentObj.find('input[type="radio"]').eq(indexNum).prop("checked"));
+                parentObj.find('input[type="radio"]').eq(indexNum).trigger("click"); //触发对应的radio的click事件                
             });
         });
     };
@@ -178,9 +168,8 @@ $.fn.ruleSingleSelect = function () {
         parentObj.children().hide(); //隐藏内容
         var divObj = $('<div class="boxwrap"></div>').prependTo(parentObj); //前插入一个DIV
         //创建元素
-        var titObj = $('<a class="select-tit" href="javascript:;"><span></span><i></i></a>').appendTo(divObj);
-        var itemObj = $('<div class="select-items"><ul></ul></div>').appendTo(divObj);
-        var arrowObj = $('<i class="arrow"></i>').appendTo(divObj);
+        var titObj = $('<a class="select-tit" href="javascript:;"><span></span><i class="icon-down-dir"></i></a>').appendTo(divObj);
+        var itemObj = $('<div class="select-items"><ul></ul></div>').appendTo(divObj);        
         var selectObj = parentObj.find("select").eq(0); //取得select对象
         //遍历option选项
         selectObj.find("option").each(function (i) {
@@ -201,17 +190,13 @@ $.fn.ruleSingleSelect = function () {
                 $(this).addClass("selected"); //添加选中样式
                 selectObj.find("option").prop("selected", false);
                 selectObj.find("option").eq(indexNum).prop("selected", true); //赋值给对应的option
-                titObj.find("span").text($(this).text()); //赋值选中值
-                arrowObj.hide();
+                titObj.find("span").text($(this).text()); //赋值选中值               
                 itemObj.hide(); //隐藏下拉框
-                selectObj.trigger("change"); //触发select的onchange事件
-                //alert(selectObj.find("option:selected").text());
+                selectObj.trigger("change"); //触发select的onchange事件                
             });
         });
         //设置样式
-        //titObj.css({ "width": titObj.innerWidth(), "overflow": "hidden" });
-        //itemObj.children("ul").css({ "max-height": $(document).height() - titObj.offset().top - 62 });
-
+        
         //检查控件是否启用
         if (selectObj.prop("disabled") == true) {
             titObj.css("cursor", "default");
@@ -224,25 +209,20 @@ $.fn.ruleSingleSelect = function () {
                 //隐藏其它的下位框菜单
                 $(".single-select .select-items").hide();
                 $(".single-select .arrow").hide();
-                //位于其它无素的上面
-                arrowObj.css("z-index", "1");
+                //位于其它无素的上面                
                 itemObj.css("z-index", "1");
-                //显示下拉框
-                arrowObj.show();
+                //显示下拉框                
                 itemObj.show();
             } else {
-                //位于其它无素的上面
-                arrowObj.css("z-index", "");
+                //位于其它无素的上面               
                 itemObj.css("z-index", "");
-                //隐藏下拉框
-                arrowObj.hide();
+                //隐藏下拉框               
                 itemObj.hide();
             }
         });
         //绑定页面点击事件
         $(document).click(function (e) {
-            selectObj.trigger("blur"); //触发select的onblure事件
-            arrowObj.hide();
+            selectObj.trigger("blur"); //触发select的onblure事件            
             itemObj.hide(); //隐藏下拉框
         });
     };
