064a8ad7cf6197bb723fb3e829b38c53f0ef44e5
jiangzheng <470192616@qq.com>
2014-12-19 11:51:51 +0800

    GetClassAttribute

--- JzSayGen/UIPageBase.cs
+++ JzSayGen/UIPageBase.cs
@@ -10,6 +10,48 @@ namespace JzSayGen
     /// </summary>
     public class UIPageBase : System.Web.UI.Page
     {
+        /// <summary>
+        /// 缓存自身类型
+        /// </summary>
+        static Type CurType = typeof(UIPageBase);
+
+        /// <summary>
+        /// 当前类型对象
+        /// </summary>
+        private Type SubType { get; set; }
+
+
+        /// <summary>
+        /// 找到当前类型对象
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        Type GetBehindType(Type t)
+        {
+            Type baseType = t.BaseType;
+            if (baseType == CurType) return t;
+            return GetBehindType(baseType);
+        }
+
+        /// <summary>
+        /// 获取类的自定义属性
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        protected T GetClassAttribute<T>() where T : Attribute
+        {
+            object[] curAttributes = this.SubType.GetCustomAttributes(typeof(T), false);
+            if (curAttributes == null || curAttributes.Length != 1) return null;
+            return curAttributes[0] as T;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public UIPageBase()
+        {
+            this.SubType = this.GetBehindType(this.GetType());            
+        }
 
         /// <summary>
         /// 
